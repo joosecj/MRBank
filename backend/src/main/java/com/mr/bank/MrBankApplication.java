2beef59d540f1c04b72053bf1c3186bb1b083965
Jose Carlos <josecarloscjj@gmail.com>
2022-09-18 17:19:15 -0300

    Security config

---
+++ backend/src/main/java/com/mr/bank/MrBankApplication.java
@@ -0,0 +1,13 @@
+package com.mr.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrBankApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/mr/bank/MrBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
